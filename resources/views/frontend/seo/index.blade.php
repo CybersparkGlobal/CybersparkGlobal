6cfc71509725ab620fdade4679478a87c334d3d3
Md Aktarozzaman <aktarozzaman8@gmail.com>
2025-11-29 18:20:51 +0600

    new page oo566

--- resources/views/frontend/seo/index.blade.php
+++ resources/views/frontend/seo/index.blade.php
@@ -1,4 +1,7 @@
 <?php
-Route::get('/seo', function () {
-    return view('frontend.seo.index');
-});
+@extends('layouts.welcome')
+
+@section('content')
+    <h1>SEO Page</h1>
+    <p>This is my new SEO page.</p>
+@endsection
